e9a565e5fa12b1abad991456d99b076ad96f6d26
Copeland <ncopelan@jomc08368.dhcp.unc.edu>
2015-04-19 22:51:02 -0400

    missed GOT ironically

--- js/script.js
+++ js/script.js
@@ -23,7 +23,9 @@
                       
                       $.each(json.data,function(i,data){
                               html += '<div class="insta-user">' + data.user.username + '</div>',
-                              html += '<img class="insta-pic" src="' + data.images.low_resolution.url + '">' 
+                              html += '<img class="insta-pic" src="' + data.images.low_resolution.url + '">',
+                              //html += '<ul class="list-inline"><li><div class="insta-user">' + data.user.username + '</div></li><li><div class="insta-comment">' + data.caption.text + '</div></li></ul>'
+                              html += '<div class="insta-comment"><strong>' + data.user.username + '</strong><small>' + data.caption.text + '</small></div>'
                       });
                       
                       console.log(html);
