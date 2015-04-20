67129fc1386abac7cb9e4f35d7da7461fd5fa209
Copeland <ncopelan@jomc08368.dhcp.unc.edu>
2015-04-19 22:07:21 -0400

    a sunday in carroll

--- js/script.js
+++ js/script.js
@@ -3,36 +3,37 @@
 //instagram
       
       
-$(function() {
-  
-  var apiurl = "https://api.instagram.com/v1/tags/gameofthrones/media/recent?access_token=265471706.94a0860.524c1ad8ed7a4c19b43574ef7803ceb3&callback=?"
-  var access_token = location.hash.split('=')[1];
-  var html = ""
-  
-          $.ajax({
-                  type: "GET",
-                  dataType: "json",
-                  cache: false,
-                  url: apiurl,
-                  success: parseData
-          });
-                          
-          
-          function parseData(json){
-                  console.log(json);
-                  
-                  $.each(json.data,function(i,data){
-                          html += '<img src="' + data.images.low_resolution.url + '">'
-                  });
-                  
-                  console.log(html);
-                  $("#results").append(html);
-                  
-          }
-          
-          
-          
-         
+    $(function() {
+      
+      var apiurl = "https://api.instagram.com/v1/tags/gameofthrones/media/recent?access_token=265471706.94a0860.524c1ad8ed7a4c19b43574ef7803ceb3&callback=?"
+      var access_token = location.hash.split('=')[1];
+      var html = ""
+      
+              $.ajax({
+                      type: "GET",
+                      dataType: "json",
+                      cache: false,
+                      url: apiurl,
+                      success: parseData
+              });
+                              
+              
+              function parseData(json){
+                      console.log(json);
+                      
+                      $.each(json.data,function(i,data){
+                              html += '<div class="insta-user">' + data.user.username + '</div>',
+                              html += '<img class="insta-pic" src="' + data.images.low_resolution.url + '">' 
+                      });
+                      
+                      console.log(html);
+                      $("#results").append(html);
+                      
+              }
+              
+              
+              
+             
 });
       
       
