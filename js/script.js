6831b22597c75ddc36546889dde8e7071aac4573
Copeland <ncopelan@jomc08368.dhcp.unc.edu>
2015-04-20 22:20:29 -0400

    mondaze

--- js/script.js
+++ js/script.js
@@ -22,13 +22,13 @@
                       console.log(json);
                       
                       $.each(json.data,function(i,data){
-                              html += '<div class="insta-user">' + data.user.username + '</div>',
+                              html += '<div class="insta-firstrow"><img class="insta-profpic" src="' + data.user.profile_picture + '"><a href="https://instagram.com/' + data.user.username + '"><strong class="insta-user">' + data.user.username + '</strong></a></div>',
                               html += '<img class="insta-pic" src="' + data.images.low_resolution.url + '">',
                               //html += '<ul class="list-inline"><li><div class="insta-user">' + data.user.username + '</div></li><li><div class="insta-comment">' + data.caption.text + '</div></li></ul>'
-                              html += '<div class="insta-comment"><strong>' + data.user.username + '</strong><small>' + data.caption.text + '</small></div>'
+                              html += '<div class="insta-comment">' + ' <a href="https://instagram.com/' + data.user.username + '"><strong> ' + data.user.username + '</strong><small></a>' + data.caption.text + '</small></div>'
                       });
                       
-                      console.log(html);
+                      //console.log(html);
                       $("#results").append(html);
                       
               }
